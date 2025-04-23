414c4615653b3b8fbebccb6bba53a1e76ce9991f
Marco Giugliani <marco.giugliani@studio.unibo.it>
2025-04-23 10:45:34 +0200

    Created the home page to go the login one

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once 'bootstrap.php';
 
 //Base Template
 $templateParams["titolo"] = "Istituto storico della Resistenza e dell'Età contemporanea di Forlì-Cesena";
-$templateParams["nome"] = "loginTemplate.php";
-$templateParams["js"] = array("js/goToLogin.js");
+$templateParams["nome"] = "home.php";
+// $templateParams["js"] = array("js/goToLogin.js");
 //$templateParams["css"] = array("css/adjustments.css");
 //$templateParams["onloadFunctions"] = "checkNotifications('$currentLanguage'); getArticlesData(true, '$currentLanguage'); getArticlesData(false, '$currentLanguage')";
 
---
+++ template/home.php
@@ -0,0 +1,3 @@
+<div class="text-center pt-5">
+    <a href="login.php">Vai al login</a>
+</div>
