fd7405d19e7a388921af8d5232fa1824f16560dc
Marco Giugliani <marco.giugliani@studio.unibo.it>
2025-05-19 18:04:55 +0200

    Resolved a bug in the adding of a new element in a resource collection where other elements already exist and added the confirm modal for the elimination of a collection element

--- js/showCurrentCollectionElements.js
+++ js/showCurrentCollectionElements.js
@@ -32,8 +32,12 @@ document.addEventListener('DOMContentLoaded', function() {
 function showRows(rows, tableHeadHtml, fields, value) {
     let rowsHtml = ``;
     if (rows.length > 0) {
-        document.querySelector('input[name="elemType"][value='+value+']').checked = true;
-        document.querySelector('input[name="elemType"]').value = value;
+        document.querySelector('input[name="elemType"][value="'+value+'"]').checked = true;
+        document.querySelectorAll('input[name="elemType"]').forEach (i => {
+            if (i.checked === false) {
+                i.disabled = true;
+            }
+        });
         rows.forEach(row => {
             rowsHtml += `<tr>`;
             fields.forEach(field => {
@@ -41,10 +45,10 @@ function showRows(rows, tableHeadHtml, fields, value) {
             });
             rowsHtml += `
                     <td class="align-middle">
-                        <a class="btn btn-secondary px-0 py-1" href="${areButtonsNotEnabled === "false" ? "modifyCollectionElement.php?id="+row['ID']+"&idPage="+document.getElementById("idPage").value+"&type="+document.querySelector('input[name="elemType"]').value : "#"}" role="button">Modifica</a>
+                        <a class="btn btn-secondary px-0 py-1" href="${areButtonsNotEnabled === "false" ? "modifyCollectionElement.php?id="+row['ID']+"&idPage="+document.getElementById("idPage").value+"&type="+document.querySelector('input[name="elemType"]:checked').value : "#"}" role="button">Modifica</a>
                     </td>
                     <td class="align-middle">
-                        <a class="btn btn-danger px-0 py-1" href="${areButtonsNotEnabled === "false" ? "../elimination/removeCollectionElement.php?id="+row['ID']+"&idPage="+document.getElementById("idPage").value+"&type="+document.querySelector('input[name="elemType"]').value : "#"}" role="button">Cancella</a>
+                        <a class="btn btn-danger px-0 py-1" href="${areButtonsNotEnabled === "false" ? "../elimination/removeCollectionElement.php?id="+row['ID']+"&idPage="+document.getElementById("idPage").value+"&type="+document.querySelector('input[name="elemType"]:checked').value : "#"}" role="button">Cancella</a>
                     </td>
                 </tr>`;   
         }); 
@@ -71,9 +75,6 @@ async function getCollectionElements(collectionID) {
                 <p class="fst-italic">Al momento non sono presenti elementi.</p>
             </div>`;
         } else {
-            document.querySelectorAll('input[name="elemType"]').forEach (i => {
-                i.disabled = true;
-            });
             document.getElementById("btnAddCollectionElem").disabled = false;
             showRows(json["bibliografia"], bibliographyElemsTableHeadHtml, ['cit'], "bibliografia");
             showRows(json["cronologia"], chronologyElemsTableHeadHtml, ['data', 'loc'], "cronologia");
