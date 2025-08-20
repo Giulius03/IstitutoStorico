48cae1d20df558788743876fc19c53fcbab25ef8
Marco Giugliani <marco.giugliani@studio.unibo.it>
2025-08-20 18:00:42 +0200

    Added the documentation to the functions.php file

--- utils/functions.php
+++ utils/functions.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Verifica che tutte le variabili passate tramite metodo POST siano settate.
+ * @return boolean True se tutte le variabili sono settate, False altrimenti.
+ */
 function checkIsSet(array $vars): bool {
     foreach ($vars as $var) {
         if (!isset($_POST[$var])) {
@@ -8,6 +12,10 @@ function checkIsSet(array $vars): bool {
     return true;
 }
 
+/**
+ * Verifica se l'admin è loggato.
+ * @return boolean True se l'admin è loggato, False altrimenti.
+ */
 function isAdminLoggedIn(): bool {
     return isset($_SESSION["email"]);
 }
